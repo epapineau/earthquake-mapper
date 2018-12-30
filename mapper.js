a2cd858d744e42e0e09c88fc5dce5bf10ce9c265
Elize Papineau <elizepapineau@gmail.com>
2018-12-30 00:45:06 -0800

    added plate boundaries

--- mapper.js
+++ mapper.js
@@ -57,6 +57,9 @@ L.mapbox.styleLayer('mapbox://styles/epapineau/cjq79h5306obj2snojh260o8v', {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     // noWrap: true
 }).addTo(map);
+var plateBounds = L.mapbox
+    .featureLayer('GeoJSON/PB2002_boundaries.json')
+    .addTo(map);
 
 // Get USGS earthquake data
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
