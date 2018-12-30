8c3112013af16734146cbd2c797142923716651b
Elize Papineau <elizepapineau@gmail.com>
2018-12-30 02:50:44 -0800

    datasets now selectable from dropdown menu

--- mapper.js
+++ mapper.js
@@ -14,7 +14,7 @@ function timeConverter(UNIX_timestamp){
     return time;
 }
 
-// Functions for determining circle attributes
+// Functions for determining circle color
 function circleColor(mag){ 
     if(mag < 2){
         // Not normally felt
@@ -37,6 +37,7 @@ function circleColor(mag){
     }
 }
 
+// Functions for determining circle size
 function circleSize(mag){
     if(mag < 2){
         // Not normally felt
@@ -59,75 +60,110 @@ function circleSize(mag){
     }
 }
 
+// Mapping function
+function mapQuakes(url){    
+    d3.json(url, function(data) {
+        // Add map background
+        var mapStyle = L.mapbox.styleLayer('mapbox://styles/epapineau/cjq79h5306obj2snojh260o8v', {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            // noWrap: true
+        }).addTo(map);
+
+        // Add plate boundries
+        var plateBounds = L.mapbox
+            .featureLayer('GeoJSON/PB2002_boundaries.json')
+            .addTo(map);
+
+        // Loop through data and add a circle for each earthquake
+        var circleLayer = [];    
+        for (var i = 0; i < data['features'].length; i++) {
+            // Extract data
+            var coords = data['features'][i]['geometry']['coordinates'];
+            var details = data['features'][i]['properties'];
+            var magnitude = details['mag'];
+            var time = timeConverter(details['time']);
+        
+            // Add circles
+            circleLayer.push(
+                L.circleMarker([coords[1], coords[0]], {
+                color: circleColor(magnitude),
+                fillOpacity: 0.5,
+                radius: circleSize(magnitude),
+                weight: 1
+                }).bindPopup(
+                    `<div class = "popup text-center p-0">
+                        <h2 class = "mb-0">${details['place']}</h2>
+                        ${time}<br>
+                        Magnitude: ${magnitude} | <a href = "${details['url']}" target = "_blank">USGS Details</a>
+                    </div>`   
+                )
+            );
+        }
+
+        // Add circles to the map
+        var allMarkers = L.layerGroup(circleLayer)
+            .addTo(map);
+    });
+}
+
+// Function to run on click of Select Data button
+function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Get form input of pettype and clear form
+    var dataType = d3.select("#inputType").node().value;
+    
+    var url; 
+    if(dataType === "hour"){
+        url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson";
+    } else if (dataType === "day"){
+        url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_day.geojson";
+    } else if (dataType === "week"){
+        url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson";
+    } else {
+        url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson";
+    }
+
+    // Clear map
+    map.eachLayer(function (layer) {
+        map.removeLayer(layer);
+        console.log(layer)
+    });
+
+    // Run mapping function with appropriate URL
+    mapQuakes(url);
+}
+
 // Initialize map and add style layer
 var map = L.map('earthquake-map', {
     minZoom: 2,
     maxZoom: 12 
 }).setView([45, -125], 4);
-L.mapbox.accessToken = 'pk.eyJ1IjoiZXBhcGluZWF1IiwiYSI6ImNqb2o0bjJwYTAxZWIza3BhNTA3NDI1MzkifQ.4vnzyiDTvF3ZceR661AR0w';
-L.mapbox.styleLayer('mapbox://styles/epapineau/cjq79h5306obj2snojh260o8v', {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    // noWrap: true
-}).addTo(map);
-var plateBounds = L.mapbox
-    .featureLayer('GeoJSON/PB2002_boundaries.json')
-    .addTo(map);
 
-// Get USGS earthquake data
-var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson';
-d3.json(url, function(data) {
-    var circleLayer = [];    
-
-    // Loop through data and add a circle for each earthquake
-    for (var i = 0; i < data['features'].length; i++) {
-        // Extract data
-        var coords = data['features'][i]['geometry']['coordinates'];
-        var details = data['features'][i]['properties'];
-        var magnitude = details['mag'];
-        var time = timeConverter(details['time']);
+var legend = L.control({position: 'bottomleft'});
     
-        // Add circle
-        circleLayer.push(
-            L.circleMarker([coords[1], coords[0]], {
-            color: circleColor(magnitude),
-            fillOpacity: 0.5,
-            radius: circleSize(magnitude),
-            weight: 1
-            }).bindPopup(
-                `<div class = "popup text-center p-0">
-                    <h2 class = "mb-0">${details['place']}</h2>
-                    ${time}<br>
-                    Magnitude: ${magnitude} | <a href = "${details['url']}" target = "_blank">USGS Details</a>
-                </div>`   
-            )
-        );
-    }
-
-    // Add circles to the map
-    L.layerGroup(circleLayer).addTo(map);
-
-    var legend = L.control({position: 'bottomleft'});
-
-    // Create legend
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend');
-        var grades = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
-        var labels = [];
-
-        // square colors and labels
-        for (var i = 0; i < grades.length; i++) {
-            // if (i === 0){
-            //     div.innerHTML +=
-            //     '<i style="background:' + circleColor(grades[i] + 1) + '"></i> ' +
-            //     grades[i] + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + '<br>' : '+');
-            // } else {
+// Create legend
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'info legend');
+            var grades = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+            var labels = [];
+    
+            // square colors and labels
+            for (var i = 0; i < grades.length; i++) {
                 div.innerHTML +=
                 '<i style="background:' + circleColor(grades[i]) + '"></i> ' +
                 (grades[i]) + (grades[i + 1] ? ' &ndash; ' + grades[i + 1] + '<br>' : '+');
-            // }
-        }
-        return div;
-    };
+            }
+            return div;
+        };
+    
+        legend.addTo(map);
 
-    legend.addTo(map);
-});
+L.mapbox.accessToken = 'pk.eyJ1IjoiZXBhcGluZWF1IiwiYSI6ImNqb2o0bjJwYTAxZWIza3BhNTA3NDI1MzkifQ.4vnzyiDTvF3ZceR661AR0w';
+
+// Default dataset on load
+mapQuakes("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson");
+
+// Change dataset on click
+d3.select("#submit").on("click", handleSubmit);
